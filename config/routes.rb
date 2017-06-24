c6e1356b3855fa3c288451f1a1388bece01453ca
Otis Milliken <otis.true@gmail.com>
2017-06-23 21:52:05 -0400

    Made contact forms

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'pages#home'
   get 'about', to: 'pages#about'
-  get 'contact', to: 'pages#contact'
+  resources :contacts
   get 'signup', to: 'pages#signup'
 end
