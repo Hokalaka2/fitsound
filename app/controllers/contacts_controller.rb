ae845fed89e5178c9646ef3e62804c061ef555ff
Otis Milliken <otis.true@gmail.com>
2017-06-29 15:01:22 -0400

    Fixed up contact page

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,6 +1,28 @@
 class ContactsController < ApplicationController
-	def index
-
+	def new
+		@contact = Contact.new;
 	end
+
+	def create
+		@contact = Contact.new(contact_params)
+
+		if @contact.save
+			name = params[:contact][:name]
+			email = params[:contact][:email]
+			body = params[:contact][:comments]
+
+			flash[:success] = "Message Sent"
+			redirect_to new_contacts_path
+		else
+			flash[:danger] = @contact.errors.full_messages.join(", ")
+			redirect_to new_contacts_path
+		end
+	end
+
+	private
+
+		def contact_params
+			params.require(:contact).permit(:name, :email, :comments)
+		end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'pages#home'
   get 'about', to: 'pages#about'
-  resources :contacts
+  resources :contacts, only: :create
   get 'signup', to: 'pages#signup'
+  get 'contact-us', to: 'contacts#new', as: 'new_contacts'
 end
