599082140294cd6200bc0f1ea223d2d4ef753367
Otis Milliken <otis.true@gmail.com>
2017-06-29 22:35:21 -0400

    Added Devise log in on Home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, path: '', path_names: { sign_in: '/', sign_out: 'logout'}
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: 'pages#home'
   get 'about', to: 'pages#about'
   resources :contacts, only: :create
   get 'signup', to: 'pages#signup'
